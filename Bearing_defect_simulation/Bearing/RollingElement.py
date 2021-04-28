45a45e5d283ab7256112ece6142f48cdb03aa2e9
Rauby, Pierrick <pierrick.rauby@gatech.edu>
2021-04-28 00:01:41 -0400

    updated docs

--- Bearing_defect_simulation/Bearing/RollingElement.py
+++ Bearing_defect_simulation/Bearing/RollingElement.py
@@ -1,12 +1,12 @@
 class RollingElement(object):
     """
-    Documentation to do
+    Rolling element class
     """
     def __init__(self,a_dB:float,a_duration:float):
         self.m_dB=a_dB # The diameter of the ball (mm)
         self.m_duration=a_duration # The duration spent by the ball in
                                    # the defect
         self.m_x_pos_in_defect=0.0 # position of the ball in the defect region
-        self.m_index_interval_touched=[]
+        self.m_index_interval_touched=[] #index touched by the ball
     def advance(self,dx):
         self.m_x_pos_in_defect+=dx
