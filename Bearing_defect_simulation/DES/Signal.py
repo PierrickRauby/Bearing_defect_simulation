ff2c1f724b1e8feca6fcd4220292df672d880530
Rauby, Pierrick <pierrick.rauby@gatech.edu>
2021-04-28 00:03:57 -0400

    Update Signal.py

--- Bearing_defect_simulation/DES/Signal.py
+++ Bearing_defect_simulation/DES/Signal.py
@@ -3,21 +3,20 @@ from scipy import fft,ifft
 
 class Signal(object):
     """
-    Documentation to do
+    Signal class
     """
     def __init__(self,a_waveform_len:int,a_waveform:np.ndarray,
             a_time_resolution:float,a_spectruum:np.ndarray):
-        """
-        Documentation to do
-        """
         self.m_waveform_len=a_waveform_len
         self.m_waveform=np.zeros(self.m_waveform_len)
         self.m_time_resolution=a_time_resolution
         self.m_spectrum=np.array([])
 
     def get_fft(self):
+        # not usefull, for futur use
         self.m_spectrum=fft(self.m_waveform_len)
         return self.m_spectrum
 
     def get_ifft(self):
+        # not usefull, for futur use
         print(shape(self.m_spectrum))
