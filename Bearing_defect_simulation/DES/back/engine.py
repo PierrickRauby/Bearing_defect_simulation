64f55c7c2c019a7a96f3423a106fab8d95af89fb
PierrickRauby <pierrick.rauby@gadz.org>
2021-04-27 22:13:13 -0400

    created requirements

--- Bearing_defect_simulation/DES/back/engine.py
+++
@@ -1,5 +0,0 @@
-# def simulate(state, event_list, verbose=True):
-    # for e in event_list:
-        # set_time(e.t)
-        # state = e.f(state, event_list)
-        # if verbose: print(f"[t={e.t}] {state}")
