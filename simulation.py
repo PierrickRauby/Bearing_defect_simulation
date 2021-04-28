9c7d2a2fb4c74ce8d9df7c2444842f528ba47770
Rauby, Pierrick <pierrick.rauby@gatech.edu>
2021-04-28 00:10:23 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -14,30 +14,33 @@ def main(a_n:int=16,a_dP:float=71.501,a_race:str='outer',
             a_lambda:np.ndarray=[0.7,0.7,0.8,0.8,0.8],
             a_delta:np.ndarray=[0.5,0,0.5,0,0.7],
             a_duration:float=1,a_frequency:float=20000,a_noise:float=0.1):
-    # Create the bearing
+    """
+    Main function of the simulation engine
+    """
+    # Check that the defect exists
     if(a_lambda==None):
         a_lambda=np.array([0.7,0.7,0.8,0.8,0.8])
     if(a_delta==None):
         a_delta=np.array([0.5,0,0.5,0,0.7])
+    # Create the Bearing
     my_bearing=Bearing(a_n=a_n,a_dP=a_dP,a_race=a_race,
             a_rpm=a_rpm,a_dB=a_dB,a_theta=a_theta,
             a_L=a_L,a_N=a_N,a_lambda=a_lambda,a_delta=a_delta)
     # Create an Acquisition
     my_acquisition=Acquisition(a_duration=a_duration,a_frequency=a_frequency,
             a_noise=a_noise)
-    #Create a simulation
+    # Create a simulation
     my_simulation=Simulation(my_bearing,my_acquisition)
-    #start a simulation 
+    # Start a simulation 
     my_simulation.start()
+    # Display the results
     my_simulation.get_results(format='show')
 
 
 
 def invoke_parser():
     """
-    Command line 
-    Positional arguments for desired paths.
-    Optional arguments for all the options. 
+    Command line argument parser
     """
 
     parser = argparse.ArgumentParser()
